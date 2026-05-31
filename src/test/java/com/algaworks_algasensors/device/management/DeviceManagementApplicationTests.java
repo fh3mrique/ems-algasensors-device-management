89d3841711adf6a9724e8b52906736424b27e8da
fh3mrique <fhemrique55@gmail.com>
2026-05-30 21:35:16 -0300

    8.13. Adicionando novas dependências no Gradle

---
+++ src/test/java/com/algaworks_algasensors/device/management/DeviceManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.algaworks_algasensors.device.management;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeviceManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
